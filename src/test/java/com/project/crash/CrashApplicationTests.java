221ee92ebc5bc8b3306b67df706a5535330fb160
sanga <tkddk9141@naver.com>
2025-05-19 18:00:20 +0900

    .

---
+++ src/test/java/com/project/crash/CrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
