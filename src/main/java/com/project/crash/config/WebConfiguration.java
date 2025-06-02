1bdb3deb8c21879e710b4c9f8de84b5e54635510
sanga <tkddk9141@naver.com>
2025-06-02 11:29:47 +0900

    feat: 유효성 검사 추가

--- src/main/java/com/project/crash/config/WebConfiguration.java
+++ src/main/java/com/project/crash/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.project.crash.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -38,7 +39,11 @@ public class WebConfiguration {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
-                        (requests) -> requests.anyRequest().permitAll())
+                        (requests) -> requests
+                                .requestMatchers(HttpMethod.POST, "/api/*/users", "/api/*/users/authenticate")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
                 .sessionManagement(
                         (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
